73dad339760927a8de47ef8e7f9b54d090baa522
gustav <st3nsson@gmail.com>
2015-05-16 17:33:07 +0200

    added get method for mock json data

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 var express = require("express");
+var mockData = require("./../mockData.json");
 var app = express();
+
 
 app.use(express.static("public"));
 app.use("/views", express.static("src/views"));
-app.get("/", function (request, response) {
-	
+app.get("/api/ads", function (request, response) {
+	console.log("getting ads");
+	response.json(mockData);
+	response.end();
 });
 
 var server = app.listen(3000, function () {
