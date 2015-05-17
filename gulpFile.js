116e42ded3b846834fe4ea2cbe5729914e00cee2
gustav <st3nsson@gmail.com>
2015-05-17 15:57:35 +0200

    Use mongo db to get ads. Also implement delete

--- gulpFile.js
+++ gulpFile.js
@@ -5,14 +5,27 @@ var concat = require("gulp-concat");
 var rename = require("gulp-rename");
 var shell = require("gulp-shell");
 var sequence = require("gulp-sequence");
-var sass = require("gulp-sass")
+var sass = require("gulp-sass");
+var mongoose = require("mongoose");
+var adsSchema = require("./models/ads");
+var mockData = require("./mockdata.json");
 
 gulp.task("start-server", ["compress", "compile-sass"], shell.task([
 	"node server/index.js"
 ]));
 
 gulp.task("test", function () {
-	
+
+});
+
+gulp.task("re-populate-db", function () {
+	mongoose.connect("mongodb://localhost/onlineAds");
+	var Ads = mongoose.model("ads");
+	Ads.find().remove();
+	for (var i = 0; i < mockData.length; i++) {
+		new Ads(mockData[i]).save();
+	}
+	return;
 });
 
 gulp.task("deploy", ["start-server"], function () {
@@ -22,6 +35,7 @@ gulp.task("deploy-dev", sequence(["compress", "watch-js", "watch-sass"], "deploy
 
 gulp.task("watch-js", function () {
 	gulp.watch("./src/js/**/*.js", ["compress"]);
+	gulp.watch("server/index.js", ["start-server"]);
 });
 
 gulp.task("watch-sass", function () {
@@ -29,14 +43,14 @@ gulp.task("watch-sass", function () {
 });
 
 gulp.task("compress", function () {
-	return gulp.src(["./src/js/app.js", "./src/js/Ads List/*","./src/js/*.js"])
-	.pipe(concat("main.js"))
-	.pipe(gulp.dest("public"));
+	return gulp.src(["./src/js/app.js", "./src/js/Ads List/*", "./src/js/*.js"])
+		.pipe(concat("main.js"))
+		.pipe(gulp.dest("public"));
 });
 
 gulp.task("compile-sass", function () {
 	gulp.src("./src/styling/**/*.scss")
-	.pipe(sass().on("error", sass.logError))
-	.pipe(concat("main.css"))
-	.pipe(gulp.dest("public"));
+		.pipe(sass().on("error", sass.logError))
+		.pipe(concat("main.css"))
+		.pipe(gulp.dest("public"));
 });
--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 var express = require("express");
-var mockData = require("./../mockData.json");
+var mongoose = require("mongoose");
+var adsModel = require("./../models/ads");
 var app = express();
-
+mongoose.connect("mongodb://localhost/onlineAds");
 
 app.use(express.static("public"));
 app.use("/views", express.static("src/views"));
+
 app.get("/api/ads", function (request, response) {
 	console.log("getting ads");
-	response.json(mockData);
-	response.end();
+	mongoose.model("ads").find(function (error, ads) {
+		response.send(ads);
+	});
+});
+
+app.delete("/api/ads/:id", function (request, response) {
+	console.log("deleting ad with id", request.params.id);
+	mongoose.model("ads").find({_id: request.params.id}).remove(function (error) {
+		if(error){
+			 response.send(error);
+		} else {
+			response.sendStatus(200);
+		}
+	});
 });
 
 var server = app.listen(3000, function () {
