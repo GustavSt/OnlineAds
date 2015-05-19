937724fd386e08e402bd8f01c0147ae0672c9fc5
gustav <st3nsson@gmail.com>
2015-05-19 22:08:21 +0200

    Start of karma config file

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+	files: [
+		"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js",
+		"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-resource.min.js",
+		"https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.min.js",
+		"https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.14/angular-ui-router.min.js",
+		"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-mocks.js",
+		"src/js/**/*.js",
+		"test/**/*.js"
+	],
+	port: 8080,
+	browser: ["PhantomJS"]
+  });
+};
